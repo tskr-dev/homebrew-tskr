313dd1ef9fa7316a006e5efba5e8e88a6ad3a128
Christian Kotait <82218860+christiankotait@users.noreply.github.com>
2025-10-05 14:00:24 +0200

    tskr 0.0.5
    Automated update via GitHub Actions

--- Formula/tskr.rb
+++ Formula/tskr.rb
@@ -1,9 +1,9 @@
 class Tskr < Formula
   desc "Task management tool for developers"
   homepage "https://github.com/tskr-dev/tskr"
-  url "https://github.com/tskr-dev/tskr/releases/download/v0.0.4/tskr-macos.tar.gz"
-  sha256 "f291f70a22d523665dceda6c5f8195aa69cd62bbacc4280238aa8aa6de07a2e8"
-  version "0.0.4"
+  url "https://github.com/tskr-dev/tskr/releases/download/v0.0.5/tskr-macos.tar.gz"
+  sha256 "d83c1103df29756a50a735ab39162423298440cfad031bd8435ad0221304e899"
+  version "0.0.5"
   license "MIT"
 
   def install
