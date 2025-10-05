e6830bfcd2052674b25d9031416cc72f5654d593
Christian Kotait <christian.kotait@gmail.com>
2025-10-05 13:02:02 +0200

    Update Tskr formula to version 0.0.4 with correct URL and SHA256 checksum

--- Formula/tskr.rb
+++ Formula/tskr.rb
@@ -1,19 +1,16 @@
 class Tskr < Formula
-    desc "Task management tool for developers"
-    homepage "https://github.com/tskr-dev/tskr"
-    url "https://github.com/tskr-dev/tskr/releases/download/v0.1.0/tskr-macos.tar.gz"
-    sha256 "REPLACE_WITH_ACTUAL_SHA256"
-    version "0.1.0"
-    license "MIT"
-  
-    def install
-      bin.install "tskr/tskr"
-      
-      # Install any additional files if needed
-      # lib.install "tskr/lib" if File.directory?("tskr/lib")
-    end
-  
-    test do
-      system "#{bin}/tskr", "--version"
-    end
+  desc "Task management tool for developers"
+  homepage "https://github.com/tskr-dev/tskr"
+  url "https://github.com/tskr-dev/tskr/releases/download/v0.0.4/tskr-macos.tar.gz"
+  sha256 "f291f70a22d523665dceda6c5f8195aa69cd62bbacc4280238aa8aa6de07a2e8"
+  version "0.0.4"
+  license "MIT"
+
+  def install
+    bin.install "tskr/tskr"
   end
+
+  test do
+    system "#{bin}/tskr", "--version"
+  end
+end
